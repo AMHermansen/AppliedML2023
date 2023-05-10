383897629ccad20cb52ba804c0a496226252079f
AMHermansen <mail@andreashermansen.dk>
2023-05-10 19:12:27 +0200

    blah

--- InitialProject/dataset.py
+++ InitialProject/dataset.py
@@ -7,31 +7,43 @@ import numpy as np
 
 class ParticleDataset(Dataset):
     def __init__(self, path="/home/amh/Documents/Coding/GitHub/AppliedML2023/data/initial/train",
-                 variables_path="/home/amh/Documents/coding/GitHub/AppliedML2023/data/initial/classification_variables.txt",
+                 variables_path="/home/amh/Documents/Coding/GitHub/AppliedML2023/data/initial/classification_variables.txt",
                  target="Truth"):
+
         if target == "ALL":
-            with open("/home/amh/Documents/coding/GitHub/AppliedML2023/data/initial/variables.txt", "r") as f:
-                target = f.read()
-            target = target.replace("\n", "").replace("'", "").split(",")
+            with open("/home/amh/Documents/Coding/GitHub/AppliedML2023/data/initial/variables.txt", "r") as f:
+                self.target_variables = f.read()
+            self.target_variables = self.target_variables.replace("\n", "").replace("'", "").replace(" ", "").split(",")
+        else:
+            self.target_variables = target
 
         with h5py.File(f"{path}.h5", "r") as f:
             data = pd.DataFrame(f[path.split('/')[-1]][:], dtype=np.float32)
 
         with open(variables_path, "r") as f:
             self.variables = f.read()
+
+
         self.variables = self.variables.replace("\n", "").replace(" ", "").replace("'", "")
         self.variables = self.variables.split(",")
 
         self.features = data[self.variables]
-        self.target = data[target]
+        self.target = data[self.target_variables]
 
         self.features = np.array(self.features)
         self.target = np.array(self.target)
-        self.features = (self.features - np.mean(self.features, axis=0)) / np.std(self.features, axis=0, ddof=1)
-        np.nan_to_num(self.features, copy=False, nan=0.0, posinf=10.0, neginf=10.0)
+        self.features = self._normalize(self.features)
+        if target == "ALL":
+            self.target = self._normalize(self.target)
 
         self.features = torch.from_numpy(self.features)
         self.target = torch.Tensor(self.target)
+
+    @staticmethod
+    def _normalize(features):
+        out = (features - np.mean(features, axis=0)) / np.std(features, axis=0, ddof=1)
+        np.nan_to_num(out, copy=False, nan=0.0, posinf=10.0, neginf=10.0)
+        return out
 
     def __getitem__(self, item):
         return self.features[item, :], self.target[item]
@@ -45,7 +57,7 @@ class ParticleDataset(Dataset):
 
 
 def main():
-    d = ParticleDataset()
+    d = ParticleDataset(target="ALL")
     print(d[42])
     print(d.features.shape)
 
