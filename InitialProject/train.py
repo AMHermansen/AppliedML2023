c02d9c0feaf0a020e609f354c88eefccb78a6d72
AMHermansen <mail@andreashermansen.dk>
2023-05-17 17:57:33 +0200

    Created initial project auto encoder

--- InitialProject/train.py
+++ InitialProject/train.py
@@ -1,14 +1,15 @@
 import lightning as L
 import torch.nn.functional
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
-from model import LightningFullyConnected, BigLightningModel
-from dataset import ParticleDataset
 from argparse import ArgumentParser
 from torch import nn
 from torch import optim
 from torch.utils.data import DataLoader
 from torch.nn import functional as F
 from lightning.pytorch.callbacks import TQDMProgressBar
+
+from model import LightningFullyConnected, BigLightningModel, LitAutoEncoder
+from dataset import ParticleDataset, AEDataset
 
 
 def single_run(
@@ -71,9 +72,42 @@ def model2_run():
                final_save="data/initial/modelbig1.ckpt")
 
 
+def ae_run(**kwargs):
+    layer_shape = [160, 80, 40, 20, 10]
+    p_dropout = 0.1
+    use_batch_norm = True
+    batch_size = 2500
+    num_workers = 2
+    final_save = "data/initial/ae_full.ckpt"
+
+    model = LitAutoEncoder(layer_shape, p_dropout, use_batch_norm=use_batch_norm)
+    train_data, val_data = AEDataset(**kwargs).split_data(0.8)
+
+    train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True,
+                                  num_workers=num_workers)
+    val_dataloader = DataLoader(val_data, batch_size=batch_size, shuffle=False,
+                                num_workers=num_workers)
+
+    print(len(val_data))
+    print(len(train_data) // batch_size)
+
+    trainer = L.Trainer(devices=2, accelerator="cpu",
+                        max_epochs=1000,
+                        callbacks=[
+                            EarlyStopping(monitor="val_loss", mode="min", patience=10),
+                            TQDMProgressBar(refresh_rate=10)],
+                        )
+    trainer.fit(model=model,
+                train_dataloaders=train_dataloader,
+                val_dataloaders=val_dataloader)
+    if final_save is not None:
+        trainer.save_checkpoint(final_save)
+
+
 def main():
     # model1_run()
-    model2_run()
+    # model2_run()
+    ae_run()
 
 
 if __name__ == "__main__":
